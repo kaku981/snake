12a8153757571fc4fde64ea43dbbbbc8905c8683
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-08 08:40:57 +0800

    Created a method to start a timer

--- main.java
+++ main.java
@@ -2,7 +2,24 @@ import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class PomodoroTimer{
+public class pomodoroTimer{
   private static Timer timer = new Timer;
-  private static TimeLeft;
+  private static TimeLef;
+}
+
+public static void startTimer(int minutes) {
+  timeLeft = minutes * 60
+  TimerTask task = new TimerTask() {
+    @Overdrive
+    public void run() {
+     if (timeLeft > 60) {
+        System.out.println("Time Left:" + timeLeft + " seconds");
+        timeLeft--;
+     }else {
+       System.out.println("Time's up! Take a Break.");
+       timer.cancel();
+      }
+    }
+  };
+  timer.scheduleAtFixedRate(task, 0, 1000);
 }
