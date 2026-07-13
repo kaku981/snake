aa1675ad72f95af91cc6e59d463f04bb146bd6d4
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-13 14:19:44 +0800

    new file:   GameFrame.java 	new file:   GamePanel.java 	new file:   SnakeGame.java 	deleted:    main.java

---
+++ GamePanel.java
@@ -0,0 +1,14 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+    GamePanel(){
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+     }
+}
